4eda8901ef7d3f7114a7f30fdcc615b2d91b3d50
PrivateRouter <101918538+PrivateRouter@users.noreply.github.com>
2023-11-26 16:29:19 -0500

    Update build_gli_bl1300.sh

--- build_gli_bl1300.sh
+++ build_gli_bl1300.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OUTPUT="$(pwd)/images"
-BUILD_VERSION="22.03.5"
+BUILD_VERSION="23.05.1"
 BOARD_NAME="ipq40xx"
 BOARD_SUBNAME="generic"
 BUILDER="https://downloads.openwrt.org/releases/${BUILD_VERSION}/targets/${BOARD_NAME}/${BOARD_SUBNAME}/openwrt-imagebuilder-${BUILD_VERSION}-${BOARD_NAME}-${BOARD_SUBNAME}.Linux-x86_64.tar.xz"
